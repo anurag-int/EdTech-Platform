e7f1d4faab456624c5b57e030fe007a9badae59a
anurag-int <ag6526803@gmail.com>
2023-06-15 01:07:57 +0530

    server initialize

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to the home Page");
+})
+
+app.listen(3000,()=>{
+    console.log("Server Started on Port 3000");
+})
