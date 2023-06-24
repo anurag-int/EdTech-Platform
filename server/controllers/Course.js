b9bdb509e8add8fb4f0072ed64f41d67411a53c8
anurag-int <ag6526803@gmail.com>
2023-06-24 14:08:54 +0530

    Create Course For Instructor Functionality Added

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -1,7 +1,6 @@
 const Course = require("../models/Course");
 const Tag = require("../models/tags");
 const User = require("../models/User");
-
 const {uploadImageToCloudinary} = require("../utils/imageUploader");
 
 
@@ -11,25 +10,91 @@ exports.createCourse = async (req, res) => {
 
     try{
         //fetch data
-        const {courserName, courseDescription, whatYouWillLearn, img} = req.body;
+        const {courseName, courseDescription, whatYouWillLearn, price, tag} = req.body;
     
         //get thumbnail
-        const thumbnail = req.files.thumbnail;
+        const thumbnail = req.files.thumbnailImage;
 
         //validation
-        if(!courserName || !courseDescription || !whatYouWillLearn || !img)
+        if(!courseName || !courseDescription || !whatYouWillLearn || !price || !tag)
         {
             return res.status(400).json({
                 success : false,
                 message : "Please Fill required details!"
             })
         }
+
+        // check for instructor
+        const userId = req.user.id;
+        const instructorDetails = await User.findById(userId);
+        console.log("Instructor Details: ", instructorDetails);
+
+        if(!instructorDetails)
+        {
+            return res.status(404).json({
+                success : false,
+                message : "Instructor Details not found"
+            })
+        }
+
+        // check given tag is valid or not
+        const tagDetails = await Tag.findById(tag);
+        if(!tagDetails)
+        {
+            return res.status(404).json({
+                success : false,
+                message : "Tag Details not found"
+            })
+        }
+
+        //Upload Image to Cloudinary
+        const thumbnailImage = await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME);
+
+        //create an entry for new course
+
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor :  instructorDetails._id,
+            whatYouWillLearn,
+            price,
+            tag : tagDetails._id,
+            thumbnail : thumbnailImage.secure_url
+        })
+
+        //  add the new course to the user schema of Instructor
+
+        await Course.findByIdAndUpdate({_id : instructorDetails._id},
+                                       {
+                                            $push:{
+                                                courses : newCourse._id
+                                            }
+                                       },
+                                       {new : true});
+        
+
+        //Updated the TAG Schema
+        await Tag.findByIdAndUpdate({_id : tagDetails._id},
+            {
+                $push : {
+                    course : newCourse._id
+                }
+            },
+            {new : true});
+        
+
+        return res.status(200).json({
+            success : true,
+            message : "Course Created Successfully",
+            data : newCourse
+        })
+         
     }
     catch(err)
     {
         return res.this.status(404).json({
             success : false,
-            message : "Try Again"
+            message : "Failed to create Course"
         })
     }
 
@@ -37,3 +102,7 @@ exports.createCourse = async (req, res) => {
 
      
 } 
+
+
+
+// getAllCourses
--- server/utils/imageUploader.js
+++ server/utils/imageUploader.js
@@ -1,16 +1,23 @@
 const cloudinary = require('cloudinary').v2;
 
 
-exports.uploadImageToCloudinary = async(file, folder, height, quality)=>{
-    const options = {folder};
-    if(height){
-        options.quality = quality;
-    }
-    if(quality)
+exports.uploadImageToCloudinary = async(file, folder, height, quality) => {
+    try{
+            const options = {folder};
+            if(height){
+                options.quality = quality;
+            }
+            if(quality)
+            {
+                options.quality = MAIL_USER
+            }
+            options.resource_type = "auto";
+    
+            return await cloudinary.uploader.upload(file.tempFilePath, options); 
+       }
+    catch(err)
     {
-        options.quality = MAIL_USER
+        return 
     }
-    options.resource_type = "auto";
-
-    return await cloudinary.uploader.upload(file.tempFilePath, options);
+    
 }
