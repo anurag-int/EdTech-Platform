aeb15948fb4887b5f03e089349d76580093edd6d
anurag-int <ag6526803@gmail.com>
2023-07-03 23:25:49 +0530

    Now User can Purchase Course(Payment Gatway Added)

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -5,6 +5,11 @@ const mailSender = require("../utils/mailSender");
 const {courseEnrollmentEmail} = require("../mail/templates/courseEnrollmentEmail");
 
 
+// 3 cases while payment
+    //1 order Initiate
+    //2 attempting payment
+    //3 paid || unpaid
+
 // capture the payment and initaiate the razorpay order
     // get courseID and userId
     // validation CourseID
@@ -98,3 +103,91 @@ exports.capturePayment = async(req, res) => {
 
 
 }
+
+// verify signature of Razorpay and Server
+
+exports.verifySignature = async(req, res) => {
+
+   const webhookSecret = "123456";
+   
+   const signature = req.headers["x-razorpay-signature"];
+
+   const shasum = crypto.createHmac("sha256", webhookSecret);
+
+   shasum.update(JSON.stringify(req.body));
+
+   const digest = shasum.digest("hex");
+
+   if(signature === digest)
+   {
+        console.log("Payment is Authorised");
+
+        const {courseId, userId} = req.body.payload.payment.entity.notes;
+
+        try{
+            // find the course in Db and enroll the student in Course
+
+            const enrolledCourse = await Course.findByIdAndUpdate(
+                                                                    {_id : courseId},
+                                                                    {
+                                                                        $push : {studentsEnrolled : userId}
+                                                                    },
+                                                                    {new : true}
+                                                                 );
+            
+            if(!enrolledCourse)
+            {
+                return res.status(500).json({
+                    success : false,
+                    message : "Course not Found"
+                });
+            }
+
+            console.log(enrolledCourse);
+
+            // find the student and add the course into its course Section.
+
+            const enrolledStudent = await User.findByIdAndUpdate(
+                                                                    {_id : userId},
+                                                                    {$push : {courses : courseId}},
+                                                                    {new : true}
+                                                                );
+
+            console.log(enrolledStudent);
+            
+            // sending mail (confirmation mail)
+            
+            const emailResponse = await mailSender(
+
+                enrolledStudent.email, 
+                "Congratulation from StudyMotion",
+                `Congratulations, Successfully Enrolled for ${enrolledCourse.couseName} Course`
+
+            );
+
+            return res.status(200).json({
+                success : true,
+                message : "Signature Verified and Course Added"
+            }); 
+
+        }   
+        catch(error)
+        {
+            return res.status(200).json({
+                success : false,
+                message : error.message
+            }); 
+        }
+   }
+   
+   else
+   {
+        return res.status(400).json({
+            success : false,
+            message : "Invalid Request"
+        })
+   }
+   
+
+}
+
