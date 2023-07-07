58cf8ae2c0fe138ae5098544569810eeac63d7e2
anurag-int <ag6526803@gmail.com>
2023-07-08 02:40:49 +0530

    cors Added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
-const PORT = 4000 || process.env.PORT;
+const cookieParser = require("cookie-parser");
+
+const courseRoutes = require("./routes/Course");
+const paymentRoutes = require("./routes/Payments");
+const profileRoutes = require("./routes/Profile");
+const userRoutes = require("./routes/User");
+
 const connect = require('./config/database');
+
+const cloudinaryConnect = require("./config/");
+const PORT = 4000 || process.env.PORT;
+
 
 app.get("/", (req, res)=>{
     res.send("Welcome to the home Page");
