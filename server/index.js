d5b9e844900a6d11c1e2be7d4f737913f678d3e2
anurag-int <ag6526803@gmail.com>
2023-07-08 02:53:01 +0530

    Required Packages Updated

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const fileUpload = require("express-fileupload");
 
 const courseRoutes = require("./routes/Course");
 const paymentRoutes = require("./routes/Payments");
 const profileRoutes = require("./routes/Profile");
 const userRoutes = require("./routes/User");
 
+const {cloudinaryConnect} = require("./config/cloudinary");
 const connect = require('./config/database');
 
-const cloudinaryConnect = require("./config/");
+require("dotenv").config();
+
 const PORT = 4000 || process.env.PORT;
+
 
 
 app.get("/", (req, res)=>{
