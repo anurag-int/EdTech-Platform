a0d26a1a950ed73999373ad8fb8eba1887a26f24
anurag-int <ag6526803@gmail.co>
2023-07-08 16:42:09 +0530

    To new function added

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -101,6 +101,35 @@ exports.deleteAccount = async (req, res) => {
     });
 };
 
+exports.updateDisplayPicture = async (req, res) => {
+    try {
+      const displayPicture = req.files.displayPicture
+      const userId = req.user.id
+      const image = await uploadImageToCloudinary(
+        displayPicture,
+        process.env.FOLDER_NAME,
+        1000,
+        1000
+      )
+      console.log(image)
+      const updatedProfile = await User.findByIdAndUpdate(
+        { _id: userId },
+        { image: image.secure_url },
+        { new: true }
+      )
+      res.send({
+        success: true,
+        message: `Image Updated successfully`,
+        data: updatedProfile,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      })
+    }
+};
+
 
 
 // a getter function to get ALL details of Any user
@@ -109,7 +138,7 @@ exports.deleteAccount = async (req, res) => {
     //return response
 
 
-exports.getAllUserDetails = async(res, res)=>{
+exports.getAllUserDetails = async(req, res)=>{
     try{
         const id = req.user.id;
 
@@ -130,3 +159,30 @@ exports.getAllUserDetails = async(res, res)=>{
         })
     }
 }
+
+
+exports.getEnrolledCourses = async (req, res) => {
+    try {
+      const userId = req.user.id
+      const userDetails = await User.findOne({
+        _id: userId,
+      })
+        .populate("courses")
+        .exec()
+      if (!userDetails) {
+        return res.status(400).json({
+          success: false,
+          message: `Could not find user with id: ${userDetails}`,
+        })
+      }
+      return res.status(200).json({
+        success: true,
+        data: userDetails.courses,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      })
+    }
+};
